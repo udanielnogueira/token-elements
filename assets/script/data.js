77d3dd2c1529907e0913d81d70b0ba63e363937c
Daniel Nogueira <danielsilvangr@gmail.com>
2024-09-05 21:33:06 -0300

    Versão 1.0

---
+++ assets/script/data.js
@@ -0,0 +1,102 @@
+let buttons = [
+  {
+    html: `<a href="assets/files/button-1.txt" class="elements__item" target="_blank"><button class="button-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path></svg><div class="button-1__text">Button</div></button></a>`,
+  },
+  {
+    html: `<a href="assets/files/button-2.txt" class="elements__item" target="_blank"><button class="button-2">Button</button></a>`,
+  },
+  {
+    html: `<a href="assets/files/button-3.txt" class="elements__item" target="_blank"><button class="button-3">Hover me :)</button></a>`,
+  },
+  {
+    html: `<a href="assets/files/button-4.txt" class="elements__item" target="_blank"><button class="button-4">Button</button></a>`,
+  },
+  {
+    html: `<a href="#" class="elements__item">Em breve mais</a>`,
+  },
+];
+
+let loaders = [
+  {
+    html: `<a href="assets/files/loader-3.txt" class="elements__item" target="_blank">
+            <div class="loader-3">
+              <div class="loader-3__dot"></div>
+              <div class="loader-3__dot"></div>
+              <div class="loader-3__dot"></div>
+              <div class="loader-3__dot"></div>
+              <div class="loader-3__dot"></div>
+              <div class="loader-3__dot"></div>
+              <div class="loader-3__dot"></div>
+              <div class="loader-3__dot"></div>
+            </div>
+          </a>`,
+  },
+  {
+    html: `<a href="assets/files/loader-4.txt" class="elements__item" target="_blank">
+            <div
+              aria-label="Orange and tan hamster running in a metal wheel"
+              role="img"
+              class="wheel-and-hamster"
+            >
+              <div class="wheel"></div>
+              <div class="hamster">
+                <div class="hamster__body">
+                  <div class="hamster__head">
+                    <div class="hamster__ear"></div>
+                    <div class="hamster__eye"></div>
+                    <div class="hamster__nose"></div>
+                  </div>
+                  <div class="hamster__limb hamster__limb--fr"></div>
+                  <div class="hamster__limb hamster__limb--fl"></div>
+                  <div class="hamster__limb hamster__limb--br"></div>
+                  <div class="hamster__limb hamster__limb--bl"></div>
+                  <div class="hamster__tail"></div>
+                </div>
+              </div>
+              <div class="spoke"></div>
+            </div>
+          </a>`,
+  },
+  {
+    html: `<a href="assets/files/loader-2.txt" class="elements__item" target="_blank">
+            <div class="loader-2">
+              <div class="loader-2-square"></div>
+              <div class="loader-2-square"></div>
+              <div class="loader-2-square"></div>
+              <div class="loader-2-square"></div>
+              <div class="loader-2-square"></div>
+              <div class="loader-2-square"></div>
+              <div class="loader-2-square"></div>
+            </div>
+          </a>`,
+  },
+  {
+    html: `<a href="assets/files/loader-5.txt" class="elements__item" target="_blank">
+      <div class="loader-5">
+        <div class="box box-1">
+          <div class="side-left"></div>
+          <div class="side-right"></div>
+          <div class="side-top"></div>
+        </div>
+        <div class="box box-2">
+          <div class="side-left"></div>
+          <div class="side-right"></div>
+          <div class="side-top"></div>
+        </div>
+        <div class="box box-3">
+          <div class="side-left"></div>
+          <div class="side-right"></div>
+          <div class="side-top"></div>
+        </div>
+        <div class="box box-4">
+          <div class="side-left"></div>
+          <div class="side-right"></div>
+          <div class="side-top"></div>
+        </div>
+      </div>
+    </a>`,
+  },
+  {
+    html: `<a href="#" class="elements__item">Em breve mais</a>`,
+  },
+];
---
+++ assets/script/script.js
@@ -0,0 +1,131 @@
+function carregar() {
+  const loading = document.querySelector(".loading");
+  const container = document.querySelector(".container");
+  const loading__text = document.querySelector(".loading__text");
+
+  setTimeout(esconder, 1490);
+  function esconder() {
+    loading.style.display = "none";
+  }
+
+  setTimeout(exibir, 1500);
+  function exibir() {
+    container.style.display = "grid";
+  }
+
+  loading__text.innerText = "Um elemento?";
+  setTimeout(() => {
+    loading__text.innerText = "Calma aí...";
+  }, 800);
+
+  function esconder() {
+    loading.style.display = "none";
+  }
+}
+
+const containerDeItens = document.querySelector(".elements__items");
+const input = document.querySelector(".hero__input");
+const tituloElementos = document.querySelector(".elements__title");
+const objeto = document.querySelector(".elements__object");
+const elemento = document.querySelector(".elemento__item");
+
+// Buttons BTN
+
+function mudarTituloBtn() {
+  tituloElementos.textContent = "Buttons";
+}
+
+function exibirButtons() {
+  for (button of buttons) {
+    containerDeItens.innerHTML += button.html;
+  }
+}
+
+function placeHolderBtn() {
+  input.placeholder = "TKBTN";
+}
+
+function exibirObjetoBtn() {
+  objeto.innerHTML = `<div class="loader-5">
+    <div class="box box-1">
+      <div class="side-left"></div>
+      <div class="side-right"></div>
+      <div class="side-top"></div>
+    </div>
+    <div class="box box-2">
+      <div class="side-left"></div>
+      <div class="side-right"></div>
+      <div class="side-top"></div>
+    </div>
+    <div class="box box-3">
+      <div class="side-left"></div>
+      <div class="side-right"></div>
+      <div class="side-top"></div>
+    </div>
+    <div class="box box-4">
+      <div class="side-left"></div>
+      <div class="side-right"></div>
+      <div class="side-top"></div>
+    </div>
+  </div>`;
+}
+
+function exibirSectionButtons() {
+  containerDeItens.innerHTML = "";
+  exibirButtons();
+  mudarTituloBtn();
+  placeHolderBtn();
+  exibirObjetoBtn();
+}
+
+// Loaders BTN
+
+function exibirLoaders() {
+  for (loader of loaders) {
+    containerDeItens.innerHTML += loader.html;
+  }
+}
+
+function placeHolderLoad() {
+  input.placeholder = "TKLOAD";
+}
+
+function mudarTituloLoader() {
+  tituloElementos.textContent = "Loaders";
+}
+
+function exibirObjetoLoad() {
+  objeto.innerHTML = `
+  <div class="loader-1">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>`;
+}
+
+function exibirSectionLoaders() {
+  containerDeItens.innerHTML = "";
+  exibirLoaders();
+  mudarTituloLoader();
+  placeHolderLoad();
+  exibirObjetoLoad();
+}
+
+// Input
+
+function exibirElementos() {
+  if (input.value == "TKBTN") {
+    exibirSectionButtons();
+  } else if (input.value == "TKLOAD") {
+    exibirSectionLoaders();
+  }
+}
+
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    exibirElementos();
+  }
+});
